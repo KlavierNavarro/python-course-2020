9d2894f7e3423c438eda56b7142b1db2724e1fc4
Klavier <klaviergn@gmail.com>
2020-08-16 11:44:38 +0200

    Exercise 00.06 - Division and remainders

---
+++ exercise0006.py
@@ -0,0 +1,6 @@
+num1 = int(input("Dime un número: "))
+num2 = int(input("Dime otro número: "))
+division = num1 / num2
+resto = num1 % num2
+print("Su división es", division)
+print("Su resto es", resto)
