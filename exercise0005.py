a8157827de43185d3f23f5fbf044be27c045ef45
Klavier <klaviergn@gmail.com>
2020-08-16 11:43:22 +0200

    Exercise 00.05 - Speed km/h m/s

---
+++ exercise0005.py
@@ -0,0 +1,3 @@
+metros = float(input("Introduce los metros recorridos: "))
+segundos = float(input("Indica cuántos segundos han sido empleados para recorrer esa distancia: "))
+print("La velociad es de", metros / segundos, "m/s o", (metros / segundos) * 3.6, "km/h")
