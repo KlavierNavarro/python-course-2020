c4f1f11a5278a5b287ac092a7ee6c63711379290
Klavier <klaviergn@gmail.com>
2020-08-16 11:45:37 +0200

    Exercise 00.07 - Pounds to ounces & pounds to grams conversor

---
+++ exercise0007.py
@@ -0,0 +1,5 @@
+libras = int(input("Introduce el peso en libras: "))
+onzas = libras * 16
+gramos = libras * 453.592
+print(libras, "libras son", onzas, "onzas")
+print(libras, "libras son", gramos, "gramos")
