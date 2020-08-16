f02aaabd2d043ca78215f9176f9da56782f9e8f9
Klavier <klaviergn@gmail.com>
2020-08-16 11:38:37 +0200

    Exercise 00.02 - Euros to pesetas (float)

--- exercise0002.py
+++ exercise0002.py
@@ -1,3 +1,3 @@
 euros = int(input("Introduce un número para convertir de euros a pesetas: "))
-pesetas = euros * 166
-print(euros, "euros son", pesetas, "pesetas")
+pesetas = int(euros * 166386)
+print(euros, "euros son", pesetas / 1000, "pesetas")
