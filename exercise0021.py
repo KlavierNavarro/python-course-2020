709eb3933941fcc166b03b3d61ef7f7fcd7f608f
Klavier <klaviergn@gmail.com>
2020-08-19 12:54:48 +0200

    Exercise 00.21 - Identity matrix

--- exercise0021.py
+++ exercise0021.py
@@ -3,7 +3,7 @@ M = []
 count = -1 
 for i in range(n): 
     M.append([0] * n) 
-    if M[n-1][n-1] == 0: 
-        for j in range(n): 
-                M[count+1][count+1] = 1 
+if M[n-1][n-1] == 0: 
+    for j in range(n): 
+        M[count+1][count+1] = 1 
 print(M)
