504c7feb37735ed0a7b4bd331f6d1765ac1a0f56
Klavier <klaviergn@gmail.com>
2020-08-19 12:50:36 +0200

    Exercise 00.21 Identity matrix

--- exercise0021.py
+++ exercise0021.py
@@ -1,5 +1,9 @@
-n = int(input("Dime un número entero positivo: "))
-M = []
-for i in range(n):
-    M.append([0] * n)
+n = int(input("Dime un número entero positivo: ")) 
+M = [] 
+count = -1 
+for i in range(n): 
+    M.append([0] * n) 
+    if M[n-1][n-1] == 0: 
+        for j in range(n): 
+                M[count+1][count+1] = 1 
 print(M)
