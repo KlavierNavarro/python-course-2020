b1781e5f6c65176a55d5cf16e1c1e4e22364544c
Klavier <klaviergn@gmail.com>
2020-08-23 17:11:54 +0200

    Exercise 00.30 - NID letter calculator

---
+++ exercise0030.py
@@ -0,0 +1,4 @@
+def letra_dni(x):
+    letra = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E']
+    return letra[x % 23]
+print(letra_dni(58386573))
